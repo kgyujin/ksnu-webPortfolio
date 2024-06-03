6445dec8205c340e999513faa404aa18a79aef95
kgyujin <k_gyujin@daum.net>
2024-06-03 21:52:27 +0900

    ✨ 프로젝트 디테일 추가

--- scripts.js
+++ scripts.js
@@ -252,15 +252,27 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "연구원들이 공지사항 및 자료를 관리하고 데이터를 효율적으로 분류 및 조회할 수 있게 지원하는 데이터 관리 웹 사이트",
             skills: ["Spring", "MySQL", "Tomcat"],
             role: "자료 등록 및 관리 기능 구현, 리포트 제작 및 관리, 서버 및 데이터베이스 관리, 보안 강화",
-            review: "엑셀 데이터 추출 등의 기능 개발을 통해 실무적 기술 능력을 한 단계 발전"
+            review: "엑셀 데이터 추출 등의 기능 개발을 통해 실무적 기술 능력을 한 단계 발전",
+            image: "img/projects/project1.png"
         },
         project2: {
             title: "대구어린이세상",
             period: "2023년 3월 31일 → 2023년 7월 26일",
-            description: "어린이와 가족들이 다양한 교육 콘텐츠와 서비스를 이용할 수 있는 웹 사이트",
+            description: "어린이와 가족들이 다양한 교육 콘텐츠와 서비스를 이용할 수 있는 웹 사이트입니다. 오픈 당시 약 1만 명 이상의 사용자가 유입되었을 정도로 높은 관심과 기대를 받은 대규모 웹 개발 프로젝트입니다. 이 프로젝트를 통해 실무 경험과 위기 대처 능력을 향상시킬 수 있었습니다.",
             skills: ["eGov", "Oracle Database", "Tomcat"],
-            role: "게시판 구현 및 유지보수, API 테스트 및 구현, 서버 및 데이터베이스 관리, 보안 강화",
-            review: "트래픽과 API 관련 다양한 예외 상황을 팀원들과 협력해 해결함으로써, 문제에 대처할 수 있는 능력 향상"
+            role: "게시판 구현 및 유지보수, API 테스트 및 구현, 서버 및 데이터베이스 관리, 보안 강화. 추가로 간편인증 API 기능이 추가됨에 따라 게시판 기능을 수정했습니다.",
+            review: "트래픽과 API 관련 다양한 예외 상황을 팀원들과 협력해 해결함으로써, 문제에 대처할 수 있는 능력 향상.",
+            issues: [
+                {
+                    title: "트래픽 과부하",
+                    description: "오픈 초기, 대량의 사용자 유입으로 인한 서버 트래픽 과부하 문제를 경험했습니다. 데이터 중복 및 예약 시스템의 에러 문제를 해결하기 위해 충분한 테스트 후 개발을 서포트했습니다."
+                },
+                {
+                    title: "API 전송 에러",
+                    description: "카카오톡 메시지 전송 시, 데이터베이스에서 사용자의 이름이 포함되지 않고 JSON 형식으로 데이터가 전송되는 오류가 발생했습니다. 이를 해결하기 위해 추가적인 테스트와 개발 지원을 진행했습니다."
+                }
+            ],
+            image: "img/projects/project2.png"
         },
         project3: {
             title: "도서 관리 프로그램",
@@ -268,7 +280,8 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "사용자가 손쉽게 도서와 회원을 관리할 수 있도록 C#으로 개발된 도서 관리 프로그램",
             skills: ["C#(WPF .NET)", "MySQL"],
             role: "전체 프로그램 설계 및 개발, 사용자 경험 개선",
-            review: "사용자가 도서 및 회원 정보를 효율적으로 관리하고, 도서의 대여 및 반납 프로세스를 손쉽게 처리할 수 있는 프로그램을 제공"
+            review: "사용자가 도서 및 회원 정보를 효율적으로 관리하고, 도서의 대여 및 반납 프로세스를 손쉽게 처리할 수 있는 프로그램을 제공",
+            image: "img/projects/project3.png"
         },
         project4: {
             title: "이무아",
@@ -276,7 +289,8 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "인공지능을 활용한 사물 인식 모바일 앱",
             skills: ["Android Studio", "Java", "TensorFlow"],
             role: "애플리케이션 개발 및 구현, PPT 제작",
-            review: "학습한 이미지들을 혼동하는 문제를 해결하기 위해 다량의 이미지를 학습시키고, 다양한 각도에서 사물을 인식할 수 있도록 개선"
+            review: "학습한 이미지들을 혼동하는 문제를 해결하기 위해 다량의 이미지를 학습시키고, 다양한 각도에서 사물을 인식할 수 있도록 개선",
+            image: "img/projects/project4.png"
         },
         project5: {
             title: "TIL",
@@ -284,7 +298,8 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "일일 학습 내용을 정리하고 Contributions에 기록을 남기며 개발 의지를 고취시킨 웹 사이드 프로젝트",
             skills: ["Node.js", "Docsify", "Markdown"],
             role: "프로젝트 전체 기획, 개발, 배포",
-            review: "Docsify 초기 사용 시 해당 기술과 관련 문서들을 학습하고 프로젝트에 적용하는 데 시간이 필요했으나 학습 동기 유지 및 개발에 대한 열정을 높일 수 있었으며, Contributions에 기록을 남기는 부수적 효과와 각 기술에 대한 이해도 향상"
+            review: "Docsify 초기 사용 시 해당 기술과 관련 문서들을 학습하고 프로젝트에 적용하는 데 시간이 필요했으나 학습 동기 유지 및 개발에 대한 열정을 높일 수 있었으며, Contributions에 기록을 남기는 부수적 효과와 각 기술에 대한 이해도 향상",
+            image: "img/projects/project5.png"
         },
         project6: {
             title: "CI3 학습 노트",
@@ -292,51 +307,49 @@ document.addEventListener('DOMContentLoaded', function() {
             description: "학습 내용을 체계적으로 되돌아볼 수 있는 웹 기반 학습 노트",
             skills: ["PHP", "CodeIgniter", "XAMPP", "MySQL"],
             role: "프로젝트 개발 및 배포, 데이터베이스 관리",
-            review: "사용자 인터페이스 사용성 개선 작업을 통해 사용자 경험 향상을 도모하고, PHP와 CodeIgniter에 대한 이해도 향상"
+            review: "사용자 인터페이스 사용성 개선 작업을 통해 사용자 경험 향상을 도모하고, PHP와 CodeIgniter에 대한 이해도 향상",
+            image: "img/projects/project6.png"
         }
     };
 
-    projectGallery.addEventListener('click', function(e) {
-        const projectElement = e.target.closest('.project');
-        if (projectElement) {
-            const projectKey = projectElement.getAttribute('data-project');
-            const project = projects[projectKey];
-            projectDetail.innerHTML = `
-                <h2>${project.title}</h2>
-                <h3>기간</h3>
-                <p>${project.period}</p>
-                <h3>소개</h3>
-                <p>${project.description}</p>
-                <h3>기술 스택</h3>
-                <p>${project.skills.join(', ')}</p>
-                <h3>역할</h3>
-                <p>${project.role}</p>
-                <h3>리뷰</h3>
-                <p>${project.review}</p>
-            `;
-            openModal();
-        }
-    });
-
-    closeButton.addEventListener('click', closeModal);
-
-    window.addEventListener('click', function(event) {
-        if (event.target === projectModal) {
-            closeModal();
-        }
-    });
-
-    function openModal() {
-        projectModal.classList.remove('fade-out');
-        projectModal.classList.add('fade-in');
+    function openModal(project) {
+        projectDetail.innerHTML = `
+            <h2>${project.title}</h2>
+            <h3>기간</h3>
+            <p>${project.period}</p>
+            <h3>소개</h3>
+            <p>${project.description}</p>
+            <h3>기술 스택</h3>
+            <p>${project.skills.join(', ')}</p>
+            <h3>역할</h3>
+            <p>${project.role}</p>
+            <h3>이슈</h3>
+            <ul>
+            ${project.issues ? project.issues.map(issue => `<li><strong>${issue.title}:</strong> ${issue.description}</li>`).join('') : ''}
+            </ul>
+            <h3>리뷰</h3>
+            <p>${project.review}</p>
+            <hr>
+            <img src="${project.image}" alt="${project.title} 이미지" style="max-width: 60%; height: auto;">
+        `;
         projectModal.style.display = 'block';
     }
 
-    function closeModal() {
-        projectModal.classList.remove('fade-in');
-        projectModal.classList.add('fade-out');
-        setTimeout(() => {
+    closeButton.addEventListener('click', () => {
+        projectModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === projectModal) {
             projectModal.style.display = 'none';
-        }, 500);
-    }
+        }
+    });
+
+    document.querySelectorAll('.project').forEach(projectElement => {
+        projectElement.addEventListener('click', () => {
+            const projectKey = projectElement.getAttribute('data-project');
+            const project = projects[projectKey];
+            openModal(project);
+        });
+    });
 });
